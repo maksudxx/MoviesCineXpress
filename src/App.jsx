4ed3516757d30c296e2eded15e150b9637826aac
Facundo Maksud <facundomaksud@gmail.com>
2025-04-12 16:48:37 -0300

    Se agrego neuva vista a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { LandingPage } from "./Pages/LandingPage/LandingPage";
 import { Routes, Route } from "react-router-dom";
-import { Login } from "./Pages/Login/Login";
+
 import { Footer } from "./components/Footer/Footer";
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
+import { Login } from "./Pages/Login/Login";
+import { Register } from "./Pages/Register/Register";
 import { Registration } from "./Pages/Registration/Registration";
 
 export const App = () => {
+  const routes = [
+    { path: "/", element: <LandingPage /> },
+    { path: "/login", element: <Login /> },
+    { path: "/signup/registration", element: <Registration /> },
+    { path: "/signup/register", element: <Register /> },
+  ];
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup/registration" element={<Registration />} />
+        {routes.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
       </Routes>
       <Footer />
     </>
