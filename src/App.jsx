c5def5778a529ebdef6d497b61de1c1fe21b4af5
Facundo Maksud <facundomaksud@gmail.com>
2025-04-13 16:12:12 -0300

    Se agrego ruta de una nueva vista para recuperar contraseñas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
 import { Login } from "./Pages/Login/Login";
+import { RecoverPassword } from "./Pages/RecoverPassword/RecoverPassword";
 import { Register } from "./Pages/Register/Register";
 import { Registration } from "./Pages/Registration/Registration";
 import { VerifyEmail } from "./Pages/VerifyEmail/VerifyEmail";
@@ -11,6 +12,7 @@ export const App = () => {
   const routes = [
     { path: "/", element: <LandingPage /> },
     { path: "/login", element: <Login /> },
+    { path: "/loginHelp", element: <RecoverPassword /> },
     { path: "/signup/registration", element: <Registration /> },
     { path: "/signup/register", element: <Register /> },
     { path: "/signup/verifyemail", element: <VerifyEmail /> },
